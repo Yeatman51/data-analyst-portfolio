bbba0cd408f3a2841bfacd7175db0720a4bb697a
jon <jonlyeatman@gmail.com>
2022-04-25 00:36:38 -0400

    Data case study page

--- client/src/components/Datacard.js
+++ client/src/components/Datacard.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+const scrollToTop = () => {
+   window.scrollTo(0, 0)
+}
+
 function Datacard(props) {
   return (
    
       <div className="dev-info">
-         <Link to={props.dataPage}>
+         <Link to={props.dataPage} onClick={scrollToTop}>
          <h1>{props.title}</h1>
          <img className="dev-pic"
          src={process.env.PUBLIC_URL + props.imgSrc} 
--- client/src/pages/Data1.js
+++ client/src/pages/Data1.js
@@ -3,6 +3,12 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Data1Pic from '../assets/img/homepage/1.png'
 import Data2Pic from '../assets/img/data-portfolio/vaccine-dashboard.png'
+import Data3Pic from '../assets/img/data-portfolio/site-data.png'
+import Data4Pic from '../assets/img/data-portfolio/pre-runsheet.png'
+import Data5Pic from '../assets/img/data-portfolio/per-site-paperwork.png'
+import Data6Pic from '../assets/img/data-portfolio/khatali.png'
+import Data7Pic from '../assets/img/data-portfolio/YCLS-page-1.png'
+import Data8Pic from '../assets/img/data-portfolio/YCLS-page-2.png'
 
 const scrollToTop = () => {
    window.scrollTo(0, 0)
@@ -27,11 +33,9 @@ function Data1() {
          alt="Data1"/>
       </div>
 
-      <div className="project-description">
-         <p>
-            This project referred to internally as the "Data Model" allows Honu team members running vaccination and testing sites across the state of Michigan to visualize vaccine vials used across the state.
-         </p>
-      </div>
+      <p className="project-description">
+         This project referred to internally as the "Data Model" allows Honu team members running vaccination and testing sites across the state of Michigan to visualize vaccine vials used across the state.
+      </p>
 
       <p className="project-description">
          This "Data Model" helps to identify patterns and trends to better utilize limited inventory for sites that have larger turnouts across the state.
@@ -41,17 +45,41 @@ function Data1() {
         The creation of this data model significantly reduced the amount of time staff members had previously been spending manually writing forms required for daily vaccination transportation.
       </p>
 
-      {/* <div className="project-pic-container">
+      <div className="project-pic-container">
          <img className="data1-pic"
-         src={Data1Pic} 
+         src={Data3Pic} 
          alt="Data1"/>
       </div>
 
       <div className="project-pic-container">
          <img className="data1-pic"
-         src={Data1Pic} 
+         src={Data4Pic} 
          alt="Data1"/>
-      </div> */}
+      </div>
+
+      <div className="project-pic-container">
+         <img className="data1-pic"
+         src={Data5Pic} 
+         alt="Data1"/>
+      </div>
+
+      <div className="project-pic-container">
+         <img className="data1-pic"
+         src={Data6Pic} 
+         alt="Data1"/>
+      </div>
+
+      <div className="project-pic-container">
+         <img className="data1-pic"
+         src={Data7Pic} 
+         alt="Data1"/>
+      </div>
+
+      <div className="project-pic-container">
+         <img className="data1-pic"
+         src={Data8Pic} 
+         alt="Data1"/>
+      </div>
 
       <div className="center">
          <button className="back-portfolio-btn" onClick={scrollToTop}><Link to="./Portfolio">back to portfolio</Link></button>
